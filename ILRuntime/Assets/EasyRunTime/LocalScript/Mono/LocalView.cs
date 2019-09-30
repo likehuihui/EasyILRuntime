43a91cc6c07d445288a9e07c023d82399d5b3fd4
741943279@qq.com <741943279@qq.com>
2019-09-30 17:13:12 +0800

    自动生成dll

---
+++ ILRuntime/Assets/EasyRunTime/LocalScript/Mono/LocalView.cs
@@ -0,0 +1,35 @@
+﻿/*
+ *    日期:
+ *    作者:
+ *    标题:
+ *    功能:
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace K.LocalWork
+{
+    public class LocalView : MonoBehaviour
+    {
+        public void Awake()
+        {
+            
+        }
+        private void OnEnable()
+        {
+
+        }
+        public void Start()
+        {
+            
+        }
+        public void OnDisable()
+        {
+            
+        }
+        public void OnDestroy()
+        {
+            
+        }
+    }
+}
